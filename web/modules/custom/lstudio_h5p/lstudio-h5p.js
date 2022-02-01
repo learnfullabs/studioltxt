297a68e959661aa23f623293d5287aa0a26f0a5e
Yasin Dahi <ymdahi@gmail.com>
2022-02-01 12:34:13 -0500

    xapi to console

--- web/modules/custom/lstudio_h5p/lstudio-h5p.js
+++ web/modules/custom/lstudio_h5p/lstudio-h5p.js
@@ -1,4 +1,6 @@
 H5P.externalDispatcher.on('xAPI', function (event) {
   parent.postMessage(JSON.stringify(event.data.statement),"*")
-  //console.log(JSON.stringify(event.data.statement));
+  console.log(event.data.statement);
 });
+
+console.log(H5P.isFramed);
